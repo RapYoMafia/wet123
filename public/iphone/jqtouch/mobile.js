f69a1be42daf321ce307d99738cd2d25b4fb83b5
xion <x@xs-MacBook-Pro.local>
2010-10-10 19:12:25 +0700

    favourites added

--- public/iphone/jqtouch/mobile.js
+++ public/iphone/jqtouch/mobile.js
@@ -1,3 +1,5 @@
+var $db = new Lawnchair({table:"appdata", adaptor:"webkit"});
+
 var jQT = new $.jQTouch({
   startupScreen:"/iphone/images/splash.png",
   statusBar:'default',
@@ -5,17 +7,13 @@ var jQT = new $.jQTouch({
   preloadImages:[
   ],
   fullScreenClass: "fullscreen",
-  // slideSelector : "slide",
   onAjaxPageLoaded: function(element){
     // for article
     jQT.init_iScroll();
-    
+
     // for gallery
     if(element.match(/gallery[0-9]+/)){
-      
-      //GENERATE GALLERY
       var gallery_id = element.replace("gallery","");
-
       var gallery_element_id = "photos" + gallery_id
       
       
@@ -28,31 +26,16 @@ var jQT = new $.jQTouch({
         })
       })
     }
+    // for all
+    var current_page = {key:element, uri:$("#" + element).attr("unique_uri"), uri_html:$("a[href=#" + element + "]").html()}
+    $("body").data("current_page", current_page);
   },
   useFastTouch: true
 });
 
-// jQuery(function(){
-//   jQT.generateGallery("happening_2",[
-//      {src:"/demos/heineken/images/happening_2/1.jpg"},
-//      {src:"/demos/heineken/images/happening_2/2.jpg"},
-//      {src:"/demos/heineken/images/happening_2/3.jpg"},
-//      {src:"/demos/heineken/images/happening_2/4.jpg"},
-//      {src:"/demos/heineken/images/happening_2/5.jpg"}
-//     ]);
-// });
-//
 
 
-/*
-$.getJSON( "/news.json", function(data){
-  for(i=0;i<data.length; i++){
-    object = data[i];
-    message_string = "<li><a class='slide' href=\"" + "/news_ajax/" + object.id + "\"><img src=\"" + object.thumbnail + "\" alt=\"\" /><h4>" + object.title + "</h4><span>" + object.category + " | " + object.date + "</span></a></li>"
-    $('#news_list').append(message_string);
-  }
-});
-*/
+// {name}
 
 
 function load_gallery(id){
@@ -63,22 +46,30 @@ function load_gallery(id){
 
 
 jQuery(function(){
-  // Add custom handler code here.
-  // $('.no_scroll').bind("touchmove", {}, function(event){
-  //   event.preventDefault();
-  // });
-  // $("#your_message").bind("focus", {}, setTextareaState);
-  //$("#gallery").bind("pageAnimationEndz", {}, function(alert(1)));
   display_lists();
 });
 
+function add_to_fav(){
+  $db.save($("body").data("current_page"), function(){
+    $('#favourites_list').html("");
+    display_fav();
+  });
+}
+
+function display_fav(){
+  $db.each(function(r){
+    document.getElementById(r.key) ? href = "#" + r.key : href = r.uri
+    var message_string= "<li><a class='slide' href='" + href + "'>" + r.uri_html.replace("/iphone/", "/iphone_thumb/") + "</a></li>";
+    $('#favourites_list').append(message_string);
+  });
+}
 
 function display_lists(){
   // DISPLAY NEWS
   $.getJSON( "/news.json", function(data){
     for(i=0;i<data.length; i++){
       object = data[i];
-      message_string = "<li><a class='slide' href=\"" + "/news_ajax/" + object.id + "\"><img src=\"" + object.thumbnail + "\" alt=\"\" /><div><h4>" + object.title + "</h4><span>" + object.category + " | " + object.date + "</span></div></a></li>"
+      var message_string = "<li><a class='slide' href=\"" + "/news_ajax/" + object.id + "\"><img src=\"" + object.thumbnail + "\" alt=\"\" /><div><h4>" + object.title + "</h4><span>" + object.category + " | " + object.date + "</span></div></a></li>"
       $('#news_list').append(message_string);
     }
   });
@@ -87,26 +78,10 @@ function display_lists(){
   $.getJSON( "/galleries.json", function(data){
     for(i=0;i<data.length; i++){
       object = data[i];
-      message_string = "<li><a class='slide' href=\"" + "/gallery_ajax/" + object.id + "\"><img src=\"" + object.thumbnail + "\" alt=\"\" /><h4>" + object.title + "</h4><span>" + object.date + "</span></a></li>"
+      var message_string = "<li><a class='slide' href=\"" + "/gallery_ajax/" + object.id + "\"><img src=\"" + object.thumbnail + "\" alt=\"\" /><h4>" + object.title + "</h4><span>" + object.date + "</span></a></li>"
       $('#gallery_list').append(message_string);
     }
   });
   
+  display_fav();
 }
-
-
-// function render_mail(data, info){
-//   $mail_container = $("#mail_container");
-//   if($mail_container.data("loaded")) return;
-//   if (info.direction == "in"){
-//     $.getJSON( "/demos/tesco/mail.json", function(data){
-//       $(".loading").fadeOut("slow");
-//       for(i=0;i<data.length; i++){
-//         var message = data[i].message;
-//         message_string = '<li><a class="cube" href="' +message.url + '"><em>&nbsp;</em><h2>' + message.subject + '</h2><span>' + message.created_at.split("T")[0] + '</span></a></li>'
-//         $('#mail_list').append(message_string);
-//       }
-//       $mail_container.data("loaded", true);
-//     });
-//   }
-// }
